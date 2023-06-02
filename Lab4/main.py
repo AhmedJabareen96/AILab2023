236b6574368a82d3f39bc4c7a46171c11a1df5e3
Ahmed Jabareen <ahmed.jabareen1@gmail.com>
2023-06-02 17:03:31 +0300

    added some fixes

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,56 +1,33 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from genetic_algorithm import GeneticAlgorithm
 from coevolution import Coevolution
 
 def main():
-    print("Sorting Network")
-    print("---------------")
-    print("1. Network of 6 elements")
-    print("2. Network of 16 elements")
-    print("Press any other key to exit")
+    num_elements = 0
+    while True:
+        choice = input("Enter the number of elements to be sorted:\n1. 6 elements\n2. 16 elements\nPress any other key to exit.\n")
+        if choice == '1':
+            num_elements = 6
+            break
+        elif choice == '2':
+            num_elements = 16
+            break
+        else:
+            return
 
-    choice = input("Enter your choice: ")
-
-    if choice == "1":
-        num_elements = 6
-    elif choice == "2":
-        num_elements = 16
-    else:
-        return
-
-    population_size = 100
-    generations = 50
-    input_vector = np.random.randint(0, 100, size=num_elements)
-
-    ga = GeneticAlgorithm(num_elements, population_size, generations)
-    best_fitness_scores = ga.evolve(input_vector)
-
-    plt.plot(best_fitness_scores)
-    plt.xlabel('Generation')
-    plt.ylabel('Best Fitness Score')
-    plt.title('Genetic Algorithm - Sorting Network')
-    plt.show()
+    population_size = 50
+    generations = 100
 
     coevolution = Coevolution(num_elements, population_size, generations)
     best_network_fitness_scores, best_vector_fitness_scores = coevolution.evolve()
 
-    plt.plot(best_network_fitness_scores, label='Best Network Fitness')
-    plt.plot(best_vector_fitness_scores, label='Best Vector Fitness')
+    # Plotting fitness scores
+    plt.plot(range(generations), best_network_fitness_scores, label='Network Fitness')
+    plt.plot(range(generations), best_vector_fitness_scores, label='Vector Fitness')
     plt.xlabel('Generation')
-    plt.ylabel('Fitness Score')
-    plt.title('Coevolution - Sorting Network')
+    plt.ylabel('Fitness')
+    plt.title('Fitness Scores over Generations')
     plt.legend()
     plt.show()
-
-    best_network = coevolution.network_population[np.argmax(best_network_fitness_scores)]
-    best_vector = coevolution.vector_population[np.argmax(best_vector_fitness_scores)]
-    best_sorted_vector = best_network.sort(best_vector)
-
-    print("Best Vector:", best_vector)
-    print("Best Sorting Network:")
-    print(best_network)
-    print("Optimal Number of Network Comparators:", best_network.num_comparators)
 
 if __name__ == '__main__':
     main()
